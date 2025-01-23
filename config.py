2d1f49d638f2778017060d7147af0eada3a99deb
Harish Lenkalapalli <dme.harishguptha@gmail.com>
2025-01-23 22:43:59 +0530

    Updated config.py and .env for database fix

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'postgresql://postgresql_localhost_qwerty123atyour_5frn_user:KqIITzJLZkiIOOYTE4s7dOLgQQn0h37J@dpg-cu96c6d6l47c73d6j9k0-a/postgresql_localhost_qwerty123atyour_5frn')
-    DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///default.db')
+    SECRET_KEY = os.getenv('SECRET_KEY','postgresql://localhost:qwerty123@your_host:5432/project' )
+    DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://localhost:qwerty123@your_host:5432/project')
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
