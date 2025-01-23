9c9752ea75fc45bfa71db5c4c5cf827b01b17d0c
Harish Lenkalapalli <dme.harishguptha@gmail.com>
2025-01-23 22:58:32 +0530

    Fixed database connection string

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('postgresql://postgres:qwerty123@5432/project')  # Make sure this line is correct
+    SQLALCHEMY_DATABASE_URI = os.getenv('postgresql://postgresql_postgres_qwerty123at5432_user:8NgAmKjVFXhs8Es8OIjySXSQlZo09kk7@dpg-cu97n0lds78s73bfhtgg-a/postgresql_postgres_qwerty123at5432')  # Make sure this line is correct
     SQLALCHEMY_TRACK_MODIFICATIONS = False
